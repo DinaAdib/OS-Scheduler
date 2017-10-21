f7ec8b938b181bc9deaaa794c14edcb1ea986565
Rana Amr Afifi <rana.amr.afifi@gmail.com>
2017-10-21 11:37:49 -0700

    Update headers.h

--- headers.h
+++ headers.h
@@ -1,3 +1,6 @@
+//
+// Created by rana-afifi on 10/21/17.
+//
 
 #include <stdio.h>      //if you don't use scanf/printf change this include
 #include <sys/types.h>
@@ -17,7 +20,7 @@
 #include <queue>
 #include <string>
 
-using namespace std; 
+using namespace std;
 
 
 #define SHKEY 300
@@ -27,22 +30,43 @@ using namespace std;
 //Struct for process data in process table block
 struct processBlock {
     int id;
-    int arrivaltime;
-    int runningtime;
+    int arrivalTime;
+    int runningTime;
     int priority;
-    int remainingtime;
-    int starttime;
-    int finishtime;
-    int waittime; //derived from other attributes?
+    int remainingTime;
+    int startTime;
+    int finishTime;
+    int waitTime; //derived from other attributes?
     string status;
 }processBlock;
 
-//Struct for process data 
+//Struct for process data
 struct processData {
     int id;
-    int arrivaltime;
-    int runningtime;
+    int arrivalTime;
+    int runningTime;
     int priority;
+    int criteria;
+
+    processData(){}
+    processData(int i , int a,int r , int p)
+    {id=i; arrivalTime=a; runningTime=r; priority=p;}
+    /* overload the less-than operator so priority queues know how to compare two processData objects */
+    bool operator>(const processData& right) const
+    {
+        return criteria < right.criteria;
+    }
+    bool operator<(const processData& right) const
+    {
+        return criteria > right.criteria;
+    }
+
+    void print() {
+        printf("\n Process Data: id %d \t arrivaltime %d runningtime %d priority %d", id, arrivalTime, runningTime,
+               priority);
+    }
+
+
 }processData;
 
 ///==============================
@@ -50,42 +74,39 @@ struct processData {
 int* shmaddr;                  //
 //===============================
 
-int nProcesses=0; 
+int nProcesses=0;
 
 int getClk()
 {
-  
-   	int clk=*shmaddr;
-       return clk;		
+
+    int clk=*shmaddr;
+    return clk;
 }
 
 
 /* All process call this function at the begining to establish communication
-between them and the clock module 
-
+between them and the clock module
 Again, Remember that the clock is only emulation
 */
 void initClk()
 {
-  int shmid = shmget(SHKEY, 4, 0444);
-  while((int)shmid == -1)
-  	{
-          //Make sure that the Clock exists
+    int shmid = shmget(SHKEY, 4, 0444);
+    while((int)shmid == -1)
+    {
+        //Make sure that the Clock exists
         printf("wait, Clock not initialized yet\n");
         sleep(1);
         shmid = shmget(SHKEY, 4, 0444);
-  	}
+    }
 
-   shmaddr = (int*) shmat(shmid, (void *)0, 0);
+    shmaddr = (int*) shmat(shmid, (void *)0, 0);
 }
 
 
 /* All process call this function at the end to release the  communication
-resources between them and the clock module 
-
-Again, Remember that the clock is only emulation 
-
-input: terminateAll : is a flag to indicate whether that 
+resources between them and the clock module
+Again, Remember that the clock is only emulation
+input: terminateAll : is a flag to indicate whether that
 this is the end of simulation it terminates all the system and release resources
 */
 
@@ -93,5 +114,5 @@ void destroyClk(bool terminateAll)
 {
     shmdt(shmaddr);
     if(terminateAll)
-      killpg(getpgrp(),SIGINT);
+        killpg(getpgrp(),SIGINT);
 }

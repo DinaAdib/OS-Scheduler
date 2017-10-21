69c6ba5da052d78453f4ee9020985f54517ecbb3
Rana Amr Afifi <rana.amr.afifi@gmail.com>
2017-10-21 12:28:32 +0200

    Added execv

--- processGenerator.cpp
+++ processGenerator.cpp
@@ -9,8 +9,15 @@ int main() {
     //TODO: 
     // 1-Ask the user about the chosen scheduling Algorithm and its parameters if exists.
     // 2-Initiate and create Scheduler and Clock processes.
+    short choice; 
+    printf("\n Choose the scheduling algorithm: \n 1-Non Pre-emptive HPF \n 2-Pre-emptive SRTN \n 3-Round Robin"); 
+    scanf("%d",&choice); 
     
-
+    int schdID=fork()
+    if (schID==0) //child executes itself 
+    {  char* const par[]={(char*)0};
+       execv("./scheduler.out",par) 
+    }
     // 3-use this function after creating clock process to initialize clock
     initClk();
     /////Toget time use the following function

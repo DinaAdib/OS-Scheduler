40342b4e52ae7a9669ba4a3620631d1dedcbf048
Rana Amr Afifi <rana.amr.afifi@gmail.com>
2017-10-21 13:34:56 +0200

    Update processGenerator.cpp
    fixed trivial errors

--- processGenerator.cpp
+++ processGenerator.cpp
@@ -9,14 +9,14 @@ int main() {
     //TODO: 
     // 1-Ask the user about the chosen scheduling Algorithm and its parameters if exists.
     // 2-Initiate and create Scheduler and Clock processes.
-    short choice; 
+    int choice; 
     printf("\n Choose the scheduling algorithm: \n 1-Non Pre-emptive HPF \n 2-Pre-emptive SRTN \n 3-Round Robin"); 
     scanf("%d",&choice); 
     
-    int schdID=fork()
-    if (schID==0) //child executes itself 
+    int schdID=fork();
+    if (schdID==0) //child executes itself 
     {  char* const par[]={(char*)0};
-       execv("./scheduler.out",par) 
+       execv("./scheduler.out",par);
     }
     // 3-use this function after creating clock process to initialize clock
     initClk();

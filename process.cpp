4e65729eed732e206a7bd3dfffda97eab362c441
Michael Malak <michael.k.malak@gmail.com>
2017-11-02 17:53:57 +0200

    Update process.cpp

--- process.cpp
+++ process.cpp
@@ -1,43 +1,48 @@
 #include "headers.h"
 
-//Signals' Handlers
-void resumeHandler(int signum);
-void pauseHandler(int signum);
-
-/* Global Variables*/
+/*----------Global variables-----------*/
 int remainingtime = 0;
 bool pauseNow=true;
 
-int main(int agrc, char* argv[]) {
-    //if you need to use the emulated clock uncomment the following line
+/*---------Functions' Headers-----*/
+void resumeHandler(int signum);
+void pauseHandler(int signum);
+
+
+int main(int agrc, char* argv[]) 
+{
+    remainingtime=atoi(argv[0]);
+    cout<<" Process: I am alive. My remaining time is "<<remainingtime<<endl;
 
     //Signals
-	remainingtime=atoi(argv[0]);
-	cout<<"I am alive!!! my remaining time is "<<remainingtime<<endl;
-     signal (SIGCONT, resumeHandler);
-     signal( SIGUSR2 ,pauseHandler);
+    signal (SIGCONT, resumeHandler); 
+    signal( SIGUSR2 ,pauseHandler);
 
-    while(remainingtime>0) {
+    while(remainingtime>0) 
+    {
+       pauseNow = true;
+       cout<<" Process: I am running now, and my remaining time="<<remainingtime<<endl;
        sleep(1);
        remainingtime--;
-       pauseNow = true;
-    printf("\n I am process child no %d , my remaining time %d \n",getpid() , remainingtime);
     }
-   printf("\n I am process child no %d , I will exit now..Bye \n",getpid());
+
+   cout<<" Process: I finished and I will exit now."<<endl;
    exit(1); //signal to indicate that process finished
 
     return 0;
 }
 
 
-void resumeHandler(int signum) {
-printf("\n I am process child no %d , I AM BACK :) \n",getpid());
-pauseNow = false;
+void resumeHandler(int signum) 
+{
+  cout<<" Process: I was paused, and now I will resume. My remaining time="<<remainingtime<<endl;
+  pauseNow = false;
 }
 
 
-void pauseHandler(int signum){
-printf("\n I am process child no %d , I will pause for now :) \n",getpid());
-if(pauseNow)
-pause();
+void pauseHandler(int signum)
+{
+
+    cout<<" Process: The scheduler sent a signal, and I will pause now."<<endl;
+    if(pauseNow){ pause(); }
 }
